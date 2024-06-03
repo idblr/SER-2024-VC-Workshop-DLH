dc0fa7ecb5c01fe4309442763f066d06b2bd341b
Ghassan Hamra <ghassan.b.hamra.phd@gmail.com>
2024-06-03 13:25:53 -0400

    suggesting changes for pull request

--- analysis code/test code.R
+++ analysis code/test code.R
@@ -8,7 +8,7 @@
 
 ## GBH made a change to the Rscript
 # @idblr was here a second time to simulate a commit conflict
-# 
+# now @idblr is here to create conditions for a merge conflict
 ## create an error when working in the same branch as partner, GBH
 
 ## creating another change in test1
